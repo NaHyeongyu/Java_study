b7859af3c1729fdee7a1cb726735c45ea9afbb09
Nahyeongyu <nahg0525@gmail.com>
2025-05-15 18:07:15 +0900

    no message

--- java_0515/src/ch01/Hello.java
+++ java_0515/src/ch01/Hello.java
@@ -2,12 +2,37 @@ package ch01;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("123");
-		
+		Person[] people = new Person[3];
+		people[0] = new Person("Hyeongyu", 22);
+		people[1] = new Person("Kangin", 24);
+		people[2] = new Person("Jisung", 23);
+
+		for (int i = 0; i < people.length; i++) {
+			people[i].sayHello(); // 기본 인사
+		}
+
+		// 오버로딩된 메서드 호출!
+		people[0].sayHello("반가워요!");
+		people[1].sayHello("오늘도 화이팅!");
 	}
 }
-public class Bow {
-	public void name() {
-		
+
+class Person {
+	String name;
+	int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// 기본 인사 메서드
+	public void sayHello() {
+		System.out.printf("Hi I'm %s and %d years old.\n", name, age);
+	}
+
+	// 오버로딩된 인사 메서드
+	public void sayHello(String message) {
+		System.out.printf("Hi I'm %s and %d years old. %s\n", name, age, message);
 	}
 }
